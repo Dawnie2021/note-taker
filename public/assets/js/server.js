99c9230e0f4dc41dcb41931e4b3ca2a32e86ea1d
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-01-08 01:53:34 -0500

    added necessary code to my server.js file

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const path = require('path');
 const PORT = 3001; 
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'viewa/index.html'));
+});
+
+
+
+app.listen(PORT, () => {
+    console.log('Example app listening at http://localhost:${PORT}');
+});
