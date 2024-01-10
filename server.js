3a3c637d8a5939ebc768bfcf2bb2fc0040a81205
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-01-09 22:00:40 -0500

    added a post request

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const dataBase = require('./db.json')
+
 
 const PORT = process.env.PORT ?? 3001; 
+
 
 const app = express();
 
@@ -17,6 +18,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/notes.html'));
+});
+
 //GET request for notes
 app.get('/api/notes', (req, res) => {
     //send message to client
@@ -24,8 +29,11 @@ app.get('/api/notes', (req, res) => {
 //log our request to the terminal 
     console.info(`${req.method} request received to get notes`);
 });
- 
-
+ //post request to add a note
+app.post('/api/notes', (req, res) => {
+    //Log that a POST request was received
+    console.info(`${req.method} request received to add a new note`);
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
